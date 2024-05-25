a215af663dd77ae081f62bb6cb10ef16b8ab3343
Amar Mesic <amar.s.mesic@gmail.com>
2024-05-25 20:19:08 +0200

    add splines to MNIST

--- KAN/KAN.py
+++ KAN/KAN.py
@@ -3,7 +3,23 @@ from KAN.KANLayer import MyKANLayer
 
 class MyKAN(nn.Module):
     
-    def __init__(self, width=None, grid=3, degree=3, approx_type="taylor", seed=69, device='cpu'):
+    def __init__(self, width=None, grid=3, degree=3, approx_type="spline", seed=69, device='cpu'):
+        """
+        width: 
+            list of integers, the width of each layer in the KAN
+        grid: 
+            int, the number of grid points to use for the spline
+        degree: 
+            int, the degree of the spline
+        approx_type: "taylor" | "spline"
+            string, the type of spline to use
+        seed: 
+            int, the seed for the random number generator
+        device: 
+            string, the device to run the KAN on
+
+        Initializes the KAN with the given parameters
+        """
         super(MyKAN, self).__init__()
 
         # intialize variables for the KAN
--- KAN/KANLayer.py
+++ KAN/KANLayer.py
@@ -1,10 +1,9 @@
 import torch.nn as nn
 import torch
-import numpy as np
 from spline import *
 from utils import *
-from scipy.interpolate import BSpline
 import matplotlib.pyplot as plt
+import spline
 
 
 
@@ -27,12 +26,12 @@ class MyKANLayer(nn.Module):
         # The spline function requires three parameters: knots, coeff, and degree
         # knots: the grid points for the spline
         # self.knots = nn.Parameter(torch.linspace(grid_range[0], grid_range[1], steps=grid + 1, device=device).repeat(size, 1)).requires_grad_(False)
-        self.knots = nn.Parameter(torch.linspace(grid_range[0], grid_range[1], steps=grid + 1, device=device), requires_grad=False)
+        self.knots = nn.Parameter(torch.linspace(grid_range[0], grid_range[1], steps=grid + 1, device=device).repeat(size, 1), requires_grad=False)
 
         # coeff: the coefficients for the spline - these are learnable!
         # I am wrapping them in a parameter since that is what they are
-        # grid + 
-        self.coeff = nn.Parameter(0.1 * torch.randn(size, 1 + degree, device=device), requires_grad=True)
+        # 
+        self.coeff = nn.Parameter(0.1 * torch.randn(size, grid + degree if approx_type == "spline" else 1 + degree, device=device), requires_grad=True)
 
 
     def forward(self, x):
@@ -49,27 +48,24 @@ class MyKANLayer(nn.Module):
 
         # store the output of the spline functions
         out = torch.zeros(self.size, batch_size)
+        
+        knots = self.knots
+        coeff = self.coeff
 
+        # print('x shape: ', x.shape)
+        # print('knots shape: ', knots.shape)
+        # print('coeff shape: ', coeff.shape)
 
-        # OLD WAY: WITH BUILT-IN B-SPLINE FUNCTION, WHICH DOES NOT PROVIDE US WITH GRADIENTS
-        # for i in range(self.size):
-        #     # x[i].shape = batch_size
-        #     spline = BSpline(self.knots, self.coeff[i].detach().numpy(), self.degree)
-        #     out[i] = torch.tensor(spline(x[i]))
+        # calcuate the output of the spline functions
+        if self.approx_type == "spline":
+            spline_values = spline.coef2curve(x, knots, coeff, self.degree)
+            out = spline_values
 
-
-        # loop through all the spline functions, and apply them to a single element for the whole batch
-        # TODO: see if we can vectorize this
-        for i in range(self.size):
-            # Use torch operations to evaluate the B-spline
-            knots = self.knots
-            coeff = self.coeff[i]
-
-            # spline_values = self.evaluate_spline(x[i], knots, coeff, self.degree)
-            # out[i] = spline_values
-
-            taylor_values = self.evaluate_taylor_series(x[i], coeff, self.degree)
-            out[i] = taylor_values
+        else:
+            # TODO: see if we can vectorize this
+            for i in range(self.size):
+                taylor_values = self.evaluate_taylor_series(x[i], coeff[i], self.degree)
+                out[i] = taylor_values
 
 
         # reshape the output to be of shape (out_dim, in_dim, batch_size)
@@ -80,55 +76,12 @@ class MyKANLayer(nn.Module):
         return y
     
 
-
-
-    def evaluate_spline(self, x, knots, coeff, degree):
-        # Implement the B-spline evaluation directly in PyTorch
-        # This is a simplified version and assumes a cubic B-spline (degree=3)
-        assert degree == 3, "This implementation only supports cubic B-splines (degree=3)"
-
-        # Initialize the B-spline basis functions
-        n_knots = len(knots)
-        n_coeffs = len(coeff)
-        # assert n_knots == n_coeffs + degree + 1, "Mismatch between number of knots and coefficients for cubic B-splines"
-
-        # Implement basis function evaluation (recursively or using a loop)
-        # This example uses a loop for simplicity
-        B = torch.zeros(x.shape[0], n_coeffs)
-
-        # Basis function calculation
-        for i in range(n_coeffs):
-            B[:, i] = self.basis_function(x, knots, i, degree)
-
-        # Evaluate the spline
-        spline_values = B.matmul(coeff)
-
-        return spline_values
-
-
-    def basis_function(self, x, knots, degree):
-        # Compute the B-spline basis function value
-        # This is a placeholder for the basis function calculation
-        pass
-
-        # You need to implement the Cox-de Boor recursion formula here
-        
-
-
     def evaluate_taylor_series(self, x, coeff, degree):
         # Evaluate the Taylor series of x using the coefficients
         exp = torch.arange(degree+1).view(1, -1).repeat(x.shape[0], 1)
         powed = torch.pow(x.view(-1, 1), exp)
         
         return torch.sum(coeff * powed, dim=1)
-
-        
-
-
-
-    
-
-
 
 
     # If we want to plot the spline curves of a layer
@@ -137,10 +90,10 @@ class MyKANLayer(nn.Module):
         points = torch.linspace(self.grid_range[0], self.grid_range[1], 100)
 
         if self.approx_type == "spline":
+            xs = points.repeat(self.size, 1)
+            y = spline.coef2curve(xs, self.knots, self.coeff, self.degree).detach().cpu().numpy()
             for i in range(self.size):
-                spline = BSpline(self.knots.cpu().numpy(), self.coeff[i].detach().cpu().numpy(), self.degree)
-                y = spline(points)
-                plt.plot(points, y, label=f'B_{i,3}(points)')
+                plt.plot(xs[i], y[i])
         else:
             for i in range(self.size):
                 y = self.evaluate_taylor_series(points, coeff=self.coeff[i], degree=self.degree).detach().cpu().numpy()
